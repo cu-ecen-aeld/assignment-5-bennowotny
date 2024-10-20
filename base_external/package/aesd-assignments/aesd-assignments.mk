00b65156a28afe7cc5a02bd2324719fedd18976d
Ben Nowotny <bennowotny65535@gmail.com>
2024-10-20 10:57:24 -0500

    use latest a3 repo

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '059f99c89aac1dd0c245ebf6b380973767d522e8'
+AESD_ASSIGNMENTS_VERSION = 'a22f0d7dba6f7e88b65e40f1ce98fd6b2b3367f7'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
